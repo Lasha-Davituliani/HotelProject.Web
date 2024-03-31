8df8f10bd42e9cf3ff028f741be8f5f8274f97f6
Lasha <Lasha@DESKTOP-GHNTHT8>
2024-04-01 02:34:05 +0400

    Add project files.

---
+++ HotelProject.Repository/ManagerRepository.cs
@@ -0,0 +1,151 @@
+﻿using HotelProject.Data;
+using HotelProject.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace HotelProject.Repository
+{
+    public class ManagerRepository
+    {
+        public async Task<List<Manager>> GetManagers()
+        {
+            List<Manager> result = new();
+            const string sqlExpression = "sp_GetAllManagers";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    await connection.OpenAsync();
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                    while (await reader.ReadAsync())
+                    {
+                        if (reader.HasRows)
+                        {
+                            result.Add(new Manager()
+                            {
+                                Id = !reader.IsDBNull(0) ? reader.GetInt32(0) : 0,
+                                FirstName = !reader.IsDBNull(1) ? reader.GetString(1) : string.Empty,
+                                LastName = !reader.IsDBNull(2) ? reader.GetString(2) : string.Empty,
+                                HotelId = !reader.IsDBNull(3) ? reader.GetInt32(3) : 0,
+                            });
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+
+                return result;
+            }
+        }
+        public async Task AddManager(Manager manager)
+        {
+            string sqlExpression = "sp_addManager";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("firstName", manager.FirstName);
+                    command.Parameters.AddWithValue("lastName", manager.LastName);
+                    command.Parameters.AddWithValue("hotelId", manager.HotelId);
+
+                    await connection.OpenAsync();
+
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new InvalidOperationException("Query didn't effect any data");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+
+        }
+        public async Task UpdateManager(Manager manager)
+        {
+            string sqlExpression = "sp_UpdateManager";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("firstName", manager.FirstName);
+                    command.Parameters.AddWithValue("lastName", manager.LastName);
+                    command.Parameters.AddWithValue("hotelId", manager.HotelId);
+                    command.Parameters.AddWithValue("id", manager.Id);
+
+                    await connection.OpenAsync();
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new InvalidOperationException("Query didn't effect any data");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+
+        }
+        public async Task DeleteManager(int id)
+        {
+            string sqlExpression = "sp_DeleteManager";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("id", id);
+
+                    await connection.OpenAsync();
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new InvalidOperationException("Query didn't effect any data");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+
+        }
+    }
+}
---
+++ HotelProject.Repository/RoomRepository.cs
@@ -0,0 +1,154 @@
+﻿using HotelProject.Data;
+using HotelProject.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace HotelProject.Repository
+{
+    public class RoomRepository
+    {
+        public async Task<List<Room>> GetRooms()
+        {
+            List<Room> result = new();
+            const string sqlExpression = "sp_GetAllRooms";
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        if (reader.HasRows)
+                        {
+                            result.Add(new Room()
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = !reader.IsDBNull(1) ? reader.GetString(1) : string.Empty,
+                                IsFree = !reader.IsDBNull(2) ? reader.GetBoolean(2) : false,
+                                HotelId = reader.GetInt32(3),
+                                DailyPrice = reader.GetDecimal(4)
+                            });
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+                return result;
+            }
+        }
+
+        public async Task AddRoom(Room room)
+        {
+            string sqlExpression = "sp_AddRoom";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("name", room.Name);
+                    command.Parameters.AddWithValue("isFree", room.IsFree ? "1" : "0");
+                    command.Parameters.AddWithValue("hotelId", room.HotelId);
+                    command.Parameters.AddWithValue("dailyPrice", room.DailyPrice);
+
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+                    if (rowsAffected == 0)
+                    {
+                        throw new InvalidOperationException("Query didn't effect any data");
+                    }
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+        }
+        public async Task UpdateRoom(Room room)
+        {
+            string sqlExpression = "sp_UpdateRoom";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new(sqlExpression, connection);
+
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("name", room.Name);
+                    command.Parameters.AddWithValue("isFree", room.IsFree);
+                    command.Parameters.AddWithValue("hotelId", room.HotelId);
+                    command.Parameters.AddWithValue("dailyPrice", room.DailyPrice);
+
+
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new InvalidOperationException("Query didn't effect any data");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+        }
+
+        public async Task DeleteRoom(int id)
+        {
+            string sqlExpression = @$"sp_DeleteRoom";
+
+            using (SqlConnection connection = new(ApplicationDbContext.ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new(sqlExpression, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("roomId", id);
+
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new InvalidOperationException("Query didn't effect any data");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                }
+            }
+
+        }
+    }
+}
